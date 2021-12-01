eaf1b02c69b35b8d190138c753bdad395b6ce944
Robin Valk <robin.valk@merapar.com>
2021-12-01 10:03:24 +0100

    Day one part two

--- days/one/main.py
+++ days/one/main.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
-    report = []
+    report = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
     # Read input
     with open("days/one/input.txt") as input_file:
@@ -7,11 +7,20 @@ if __name__ == "__main__":
 
     # Count times measurment increased
     increases = 0
-    last_measurement = report[0]
-    for measurement in report[1:]:
-        if last_measurement <= measurement:
+    last_measurement = None
+    for index, mes in enumerate(report):
+        if last_measurement is None:
+            last_measurement = sum(report[:3])
+            continue
+
+        window = report[index:(index+3)]
+        if len(window) < 3:
+            break
+
+        new_sum = sum(window)
+        if new_sum > last_measurement:
             increases+=1
 
-        last_measurement = measurement
+        last_measurement = new_sum
 
     print(increases)
