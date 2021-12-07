e6d48322e769ef1bcab6ad51e859363ab36f39f7
Robin Valk <robin.valk@merapar.com>
2021-12-07 13:40:10 +0100

    Day six part two

--- days/six/main.py
+++ days/six/main.py
@@ -5,15 +5,27 @@ if __name__ == "__main__":
     with open("days/six/input.txt") as input_file:
         input = input_file.read()
 
-    num_days = 80
-    school = [int(x) for x in input.strip().split(",")]
+    num_days = 256
+    counts = [int(x) for x in input.strip().split(",")]
+    school = {}
 
+    for fish in counts:
+        if school.get(fish) == None:
+            school[fish] = 1
+        else:
+            school[fish] = 1 + school.get(fish)
+
+    last_run = school
     for i in range(num_days):
-        for fishIndex, daysLeft in enumerate(school):
-            if daysLeft == 0:
-                school[fishIndex] = 7
-                school.insert(0, 8)
-            else:
-                school[fishIndex] = school[fishIndex] - 1
+        new_run = {}
 
-    print(len(school))
+        for days, num in last_run.items():
+            if days != 0:
+                new_run[days - 1] = new_run.get(days - 1, 0) + last_run.get(days, 0)
+            else:
+                new_run[6] = new_run.get(6, 0) + num
+                new_run[8] = num
+
+        last_run = new_run
+
+    print(sum(last_run.values()))
