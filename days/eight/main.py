09dc689c10e1a372a997772fed810e4959c95d78
Robin Valk <robin.valk@merapar.com>
2021-12-09 17:07:57 +0100

    Day eight part two

--- days/eight/main.py
+++ days/eight/main.py
@@ -15,15 +15,55 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce'''
 
     # Read input
-    with open("days/eight/input.txt") as input_file:
-        input = input_file.read()
+    # with open("days/eight/input.txt") as input_file:
+    #     input = input_file.read()
 
     count = 0
     unique_digit_numbers = [2, 4, 3, 7]
 
-    for line in input.strip().split("\n"):
-        signal_pattern, output_value = line.strip().split(" | ")
-        digits = [int(len(x)) for x in output_value.strip().split(" ")]
-        count += len(list(filter(lambda x: x in unique_digit_numbers, digits)))
+    results = []
 
-    print(count)
+    for line in input.strip().split("\n"):
+        digits = {}
+        segments = {}
+        signal_pattern, output_value = line.strip().split(" | ")
+
+        digit_mapping = {}
+        signals = signal_pattern.strip().split(" ")
+        signals_to_process = signals
+        while len(digit_mapping) < 10:
+            for signal in signals_to_process:
+                signal_segments = ''.join(sorted(list(signal)))
+
+                if len(signal_segments) == 2:
+                    digit_mapping[1] = signal_segments
+                if len(signal_segments) == 4:
+                    digit_mapping[4] = signal_segments
+                if len(signal_segments) == 3:
+                    digit_mapping[7] = signal_segments
+                if len(signal_segments) == 7:
+                    digit_mapping[8] = signal_segments
+                if len(signal_segments) == 5 and digit_mapping.get(1) != None:
+                    if len(set(signal_segments).intersection(set(digit_mapping.get(1, ())))) == 2:
+                        digit_mapping[3] = signal_segments
+                    elif len(set(signal_segments).intersection(set(digit_mapping.get(6, ())))) == 5:
+                        digit_mapping[5] = signal_segments
+                    else:
+                        digit_mapping[2] = signal_segments
+                if len(signal_segments) == 6:
+                    if len(set(signal_segments).intersection(set(digit_mapping.get(4, ())))) == 4:
+                        digit_mapping[9] = signal_segments
+                    elif len(set(signal_segments).intersection(set(digit_mapping.get(1, ())))) == 2:
+                        digit_mapping[0] = signal_segments
+                    else:
+                        digit_mapping[6] = signal_segments
+
+
+        number = ''
+        for output_number in [''.join(sorted(x)) for x in output_value.strip().split(" ")]:
+            key, value = list(filter(lambda x: x[1] == output_number, digit_mapping.items()))[0]
+            number += str(key)
+
+        results.append(int(number))
+
+    print(sum(results))
