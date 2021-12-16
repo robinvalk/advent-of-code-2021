82a5b6982a8115e429de40a6001ce371f7202b3c
Robin Valk <robin.valk@merapar.com>
2021-12-16 12:03:19 +0100

    Day ten part two

--- days/ten/main.py
+++ days/ten/main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from math import floor
 
 if __name__ == "__main__":
     input = '''[({(<(())[]>[[{[]{<()<>>
@@ -20,9 +21,13 @@ if __name__ == "__main__":
     lines = [list(line.strip()) for line in input.split("\n")]
     failing_characters = defaultdict(int)
 
+    scores = []
+    scoring = {'}': 3, ')': 1, '>': 4, ']': 2}
     for y, line in enumerate(lines):
+        line_sum = 0
         closing_characters_expected = []
 
+        line_corrupt = False
         for x, character in enumerate(line):
             is_opening_character = character in opening_characters.keys()
 
@@ -32,12 +37,19 @@ if __name__ == "__main__":
 
             expected_closing_character = closing_characters_expected.pop()
             if expected_closing_character != character:
-                failing_characters[character] += 1
+                line_corrupt = True
                 break
 
-    sum = 0
-    scoring = {'}': 1197, ')': 3, '>': 25137, ']': 57}
-    for character, count in failing_characters.items():
-        sum += count * scoring[character]
+        if line_corrupt:
+            continue
 
-    print(sum)
+        for closing_character in reversed(closing_characters_expected):
+            line_sum = line_sum * 5 + scoring[closing_character]
+
+        scores.append(line_sum)
+
+    scores.sort()
+
+    print(scores)
+
+    print(scores[floor(len(scores) / 2)])
